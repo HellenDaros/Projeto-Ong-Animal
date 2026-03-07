2eb95166de89b311e6c7da91e74a7c5e19b0073d
HELLEN CARVALHO DAROS <hellen.daros@alunos.sc.senac.br>
2026-03-06 21:54:49 -0300

    ajuste na sidebar

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,17 +2,18 @@
 import { useAuth } from "../context/AuthContext";
 
 export default function Header() {
+  const { usuario, logout } = useAuth();
 
-  const{usuario, logout} = useAuth();
   return (
-   <header className="fixed w-full top-0 z-50 bg-[#f1f5f4] border-b border-stone-200 px-6 py-3">
+    /* REMOVIDO: fixed, w-full, top-0 */
+    /* MANTIDO: bg, border e paddings */
+    <header className="bg-[#f1f5f4] border-b border-stone-200 px-6 py-3">
+      {/* O max-w-7xl garante que o conteúdo interno não espalhe mais que a galeria */}
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         
-        {/* Lado Esquerdo: Perfil Samuel */}
+        {/* Lado Esquerdo: Perfil */}
         <div className="flex items-center gap-3">
-          {/* Círculo do Perfil: Laranja Fixo (orange-500) */}
           <div className="w-10 h-10 rounded-full bg-orange-500 flex items-center justify-center shadow-sm">
-            {/* Ícone de Usuário SVG (w3.org) */}
             <svg 
               xmlns="http://www.w3.org/2000/svg" 
               viewBox="0 0 24 24" 
@@ -29,7 +30,7 @@ export default function Header() {
           </div>
           <div className="flex flex-col">
             <span className="text-slate-800 font-black text-base tracking-tight leading-tight">
-              {usuario?.name.toLocaleUpperCase()||"Usuario indefinido"}
+              {usuario?.name.toLocaleUpperCase() || "USUÁRIO INDEFINIDO"}
             </span>
             <span className="text-teal-600 text-[10px] font-bold uppercase tracking-wider">
               Painel ONG
@@ -37,11 +38,9 @@ export default function Header() {
           </div>
         </div>
 
-        {/* Lado Direito: Botão Sair Estilo "Acesso ONG" */}
+        {/* Lado Direito: Botão Sair */}
         <button type="button" className="group relative flex items-center" onClick={logout}>
-          {/* Fundo Slate, muda para Laranja (orange-500) no Hover */}
           <div className="flex items-center gap-2 bg-[#1e293b] hover:bg-orange-500 text-white pl-5 pr-8 py-2.5 rounded-full font-bold transition-all duration-300 shadow-lg z-10">
-            {/* Ícone de Logout SVG (w3.org) */}
             <svg 
               xmlns="http://www.w3.org/2000/svg" 
               viewBox="0 0 24 24" 
